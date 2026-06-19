f2b8dd8112a7bdfd1a62756c4a874b95b90953fc
Oracle <15791794+OracleNine@users.noreply.github.com>
2026-06-19 13:09:56 -0400

    Imported models from Resonance, created env file

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField("self")
+
+class Wiki(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from core.models import Wiki, Profile
 
-# Create your models here.
+class Role(models.Model):
+    wiki = models.ForeignKey(Wiki, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
+    PERM_FOUNDER = models.BooleanField()
+
+class Member(models.Model):
+    wiki = models.ForeignKey(Wiki, on_delete=models.CASCADE)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    roles = models.ManyToManyField(Role)
+
+class Tag(models.Model):
+    wiki = models.ForeignKey(Wiki, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+class Page(models.Model):
+    wiki = models.ForeignKey(Wiki, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
+    content = models.TextField()
+    watchlist = models.ManyToManyField(Profile)
+    tags = models.ManyToManyField(Tag)
+
+class Revision(models.Model):
+    target = models.ForeignKey(Page, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(Tag)
