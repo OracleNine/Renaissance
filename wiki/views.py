cb5474b9a9c425c3369c215e17171f42d732cf6d
Oracle <15791794+OracleNine@users.noreply.github.com>
2026-06-21 01:37:17 -0400

    Fixed mistake where home would never be generated

--- wiki/views.py
+++ wiki/views.py
@@ -4,36 +4,35 @@ from wiki.models import Page
 
 # Create your views here.
 
-def pageExists(wiki_name, page_name):
+def page_exists(wiki_name, page_name):
     wiki = Wiki.objects.filter(subdomain=wiki_name)
     if not wiki.exists():
-        return {"pageExists": False}
+        return {}
     else:
         wiki = wiki[0]
         name = page_name.replace("_", " ")
         pageExists = Page.objects.filter(name=name).exists()
 
         if not pageExists:
-            return {"pageExists": False}
+            return {}
         else:
             page = Page.objects.filter(name=name)[0]
             return {
-                "pageExists": True,
                 "page": page,
                 "wiki": wiki
             }
 
 def page(request, wiki_name, page_name):
-    result = pageExists(wiki_name, page_name)
-    if not result['pageExists']:
+    pageExists = page_exists(wiki_name, page_name)
+    if not pageExists:
         return redirect("/404")
-    elif (not result['pageExists']) and (result['page'].name == "Home"):
+    elif pageExists["page"].name == "Home":
         # First time wiki setup
-        homePage = Page(wiki=result['wiki'], name="Home", content="Welcome to your new wiki!")
+        homePage = Page(wiki=pageExists["wiki"], name="Home", content="Welcome to your new wiki!")
         # TODO: Generate top and side navigation
         homePage.save()
 
-    page = Page.objects.filter(wiki=result['wiki'], name=result['page'].name)[0]
+    page = Page.objects.filter(wiki=pageExists["wiki"], name=pageExists["page"].name)[0]
     context = page.createDict()
     return render(request, "wiki/page/page.html", context={"page": context})
 
