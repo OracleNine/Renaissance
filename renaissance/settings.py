2044d59b54a65ec258cfff09ec62c724968eb461
Oracle <15791794+OracleNine@users.noreply.github.com>
2026-06-20 22:19:27 -0400

    Added HTMX

--- renaissance/settings.py
+++ renaissance/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'wiki.apps.WikiConfig',
     'crispy_forms',
-    'crispy_bootstrap5'
+    'crispy_bootstrap5',
+    'django_htmx'
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_htmx.middleware.HtmxMiddleware"
 ]
 
 ROOT_URLCONF = 'renaissance.urls'
