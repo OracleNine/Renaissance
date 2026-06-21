fe9bb2528bc56a7e58aa4b1d566571d744593319
Oracle <15791794+OracleNine@users.noreply.github.com>
2026-06-21 02:01:23 -0400

    Moved page existence function to a separate utils file

--- wiki/views.py
+++ wiki/views.py
@@ -1,45 +1,21 @@
 from django.shortcuts import render, redirect
-from core.models import Wiki
 from wiki.models import Page
+from .utils import page_exists
 
 # Create your views here.
 
-def page_exists(wiki_name, page_name):
-    wiki = Wiki.objects.filter(subdomain=wiki_name)
-    if not wiki.exists():
-        return {
-            "wiki": None, 
-            "page": None
-            }
-    else:
-        wiki = wiki[0]
-        name = page_name.replace("_", " ")
-        pageExists = Page.objects.filter(name=name).exists()
-
-        if not pageExists:
-            return {
-                "wiki": wiki,
-                "page": None
-            }
-        else:
-            page = Page.objects.filter(name=name)[0]
-            return {
-                "page": page,
-                "wiki": wiki
-            }
-
 def page(request, wiki_name, page_name):
-    pageExists = page_exists(wiki_name, page_name)
-    if pageExists['wiki'] is None:
+    result = page_exists(wiki_name, page_name)
+    if result['wiki'] is None:
         return redirect("/404")
-    elif pageExists['page'] is None:
-        return redirect("/wiki/" + pageExists['wiki'].subdomain) # TODO change this to a customizeable 404 page
-    elif pageExists["page"].name == "Home":
+    elif result['page'] is None:
+        return redirect("/wiki/" + result['wiki'].subdomain) # TODO change this to a customizeable 404 page
+    elif result["page"].name == "Home":
         # First time wiki setup
-        homePage = Page(wiki=pageExists["wiki"], name="Home", content="Welcome to your new wiki!")
+        homePage = Page(wiki=result["wiki"], name="Home", content="Welcome to your new wiki!")
         homePage.save()
 
-    page = Page.objects.filter(wiki=pageExists["wiki"], name=pageExists["page"].name)[0]
+    page = Page.objects.filter(wiki=result["wiki"], name=result["page"].name)[0]
     context = page.createDict()
     return render(request, "wiki/page/page.html", context={"page": context})
 
