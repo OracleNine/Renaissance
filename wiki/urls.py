9b4c2fe618b2536da96f0d5a1d4d71b32458c4f4
Oracle <15791794+OracleNine@users.noreply.github.com>
2026-06-22 01:25:47 -0400

    Changed revision URL slightly

--- wiki/urls.py
+++ wiki/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("<str:wikiSubdomain>/", views.index, name="wiki_index"),
     path("<str:wikiSubdomain>/p/<str:pageName>", views.page, name="wiki_page"),
     path("<str:wikiSubdomain>/p/<str:pageName>/edit", views.edit, name="wiki_edit"),
-    path("<str:wikiSubdomain>/p/<str:pageName>/api/view_revisions", views.view_revisions, name="wiki_view_revisions"),
+    path("<str:wikiSubdomain>/p/<str:pageName>/revision", views.view_revisions, name="wiki_view_revisions"),
 ]
