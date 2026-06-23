482a8cc2f7cb6cfeeb0b7c38063768911e713fa8
Oracle <15791794+OracleNine@users.noreply.github.com>
2026-06-22 23:50:56 -0400

    Lexical and CKEditor didnt work, need to look for alternatives

--- assets/js/wiki-editor.js
+++
@@ -1,7 +0,0 @@
-import { Editor } from '@tiptap/core'
-import StarterKit from '@tiptap/starter-kit'
-
-new Editor({
-  element: document.querySelector('.test-editor'),
-  extensions: [StarterKit],
-})
