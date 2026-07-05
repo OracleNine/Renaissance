9cafc7d335ac3203cdcc392c9435bdd0f8d258d3
Oracle Nine <15791794+OracleNine@users.noreply.github.com>
2026-07-05 18:46:03 -0400

    Navbar works on mobile and desktop

--- client/src/app/core/navigation/side-navigation/side-navigation.ts
+++ client/src/app/core/navigation/side-navigation/side-navigation.ts
@@ -1,15 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from "@angular/router";
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-side-navigation',
-  imports: [RouterOutlet, RouterLink, MatIconModule, MatTooltipModule],
+  imports: [RouterOutlet, RouterLink, MatIconModule, MatTooltipModule, MatExpansionModule, MatListModule],
   templateUrl: './side-navigation.html',
   styleUrl: './side-navigation.css',
 })
 export class SideNavigation {
-
-
+  
 }
