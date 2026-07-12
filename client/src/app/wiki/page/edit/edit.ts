2b157970e3ecbac338455cfe86afb3035ebffc4b
Oracle Nine <15791794+OracleNine@users.noreply.github.com>
2026-07-12 17:14:15 -0400

    Removing domternal, I'll just make an editor myself

--- client/src/app/wiki/page/edit/edit.ts
+++ client/src/app/wiki/page/edit/edit.ts
@@ -1,25 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
-import { Wysiwyg } from './wysiwyg/wysiwyg';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
-  imports: [Wysiwyg],
+  imports: [],
   templateUrl: './edit.html',
   styleUrl: './edit.css',
 })
 export class Edit {
   protected route = inject(ActivatedRoute)
-  name = signal('')
-  content = signal('')
-  tags = signal([])
-  ngOnInit() {
-    this.route.data.subscribe((response: any) => {
-      if (response.page) {
-        this.name.set(response.page.name)
-        this.content.set(response.page.content)
-        this.tags.set(response.page.tags)
-      }
-    })
-  }
 }
