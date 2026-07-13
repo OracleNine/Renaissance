2f77b255f0e889e3463689e71a7b85eff8c04a78
Oracle Nine <15791794+OracleNine@users.noreply.github.com>
2026-07-13 02:24:14 -0400

    Back to domternal

--- client/src/app/wiki/page/edit/edit.ts
+++ client/src/app/wiki/page/edit/edit.ts
@@ -1,46 +1,18 @@
+import { Component, signal } from '@angular/core'
 import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  Inject,
-  PLATFORM_ID,
-  ViewChild
-} from '@angular/core';
-
-import { isPlatformBrowser } from '@angular/common';
+  DomternalEditorComponent,
+  DomternalToolbarComponent,
+  DomternalBubbleMenuComponent,
+} from '@domternal/angular'
+import { Editor, StarterKit, BubbleMenu } from '@domternal/core'
 
 @Component({
-  selector: 'app-editor',
-  standalone: true,
-  template: `<div #editor></div>`
+  selector: 'app-edit',
+  imports: [DomternalEditorComponent, DomternalToolbarComponent, DomternalBubbleMenuComponent],
+  templateUrl: './edit.html',
 })
-export class Edit implements AfterViewInit {
-
-  @ViewChild('editor', { static: true })
-  editor!: ElementRef<HTMLDivElement>;
-
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
-
-  async ngAfterViewInit() {
-
-    if (!isPlatformBrowser(this.platformId)) {
-      return;
-    }
-
-    const { EditorState } = await import('@codemirror/state');
-    const { EditorView } = await import('@codemirror/view');
-    const { basicSetup } = await import('codemirror');
-
-    const state = EditorState.create({
-      doc: '',
-      extensions: [basicSetup]
-    });
-
-    new EditorView({
-      state,
-      parent: this.editor.nativeElement
-    });
-  }
+export class Edit {
+  editor = signal<Editor | null>(null)
+  extensions = [StarterKit, BubbleMenu]
+  content = '<p>Hello from Angular!</p>'
 }
