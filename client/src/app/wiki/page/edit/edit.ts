11d62cfc951919bb895f31d89df4e846b850c60d
Oracle Nine <15791794+OracleNine@users.noreply.github.com>
2026-07-13 00:40:59 -0400

    Added CodeMirror as main text editor

--- client/src/app/wiki/page/edit/edit.ts
+++ client/src/app/wiki/page/edit/edit.ts
@@ -1,12 +1,46 @@
-import { Component, inject, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  PLATFORM_ID,
+  ViewChild
+} from '@angular/core';
+
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
-  selector: 'app-edit',
-  imports: [],
-  templateUrl: './edit.html',
-  styleUrl: './edit.css',
+  selector: 'app-editor',
+  standalone: true,
+  template: `<div #editor></div>`
 })
-export class Edit {
-  protected route = inject(ActivatedRoute)
+export class Edit implements AfterViewInit {
+
+  @ViewChild('editor', { static: true })
+  editor!: ElementRef<HTMLDivElement>;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  async ngAfterViewInit() {
+
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
+
+    const { EditorState } = await import('@codemirror/state');
+    const { EditorView } = await import('@codemirror/view');
+    const { basicSetup } = await import('codemirror');
+
+    const state = EditorState.create({
+      doc: '',
+      extensions: [basicSetup]
+    });
+
+    new EditorView({
+      state,
+      parent: this.editor.nativeElement
+    });
+  }
 }
